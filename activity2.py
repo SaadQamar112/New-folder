5ba6cfd6400e427ee4dcfc3db77e0f1a4ad39d03
SaadQamar112 <saadbinadeem@gmail.com>
2025-03-25 19:37:07 +0500

    activity 2

---
+++ activity2.py
@@ -0,0 +1,22 @@
+#assigining different variables
+
+name="tom"
+age=8
+is_student=False
+weight=6.5
+
+#printing variables
+
+print("name:",name)
+print ("",type (name))
+print ("age :",age)
+print ("",type (age) )
+print ("is_student :",is_student)
+print (type (is_student))
+print ("weight :",weight)
+print (type (weight))
+
+#typecasting converting data types
+
+weight=int (weight)
+print (type (weight))
