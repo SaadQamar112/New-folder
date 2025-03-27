536f113b2e2a550448fad62bc3794c31e3165801
SaadQamar112 <saadbinadeem@gmail.com>
2025-03-27 19:10:33 +0500

    HW 4

---
+++ HW/m.py
@@ -0,0 +1,7 @@
+#taking input from the user
+print("Enter a number")
+number=int(input("enter the number:"))
+
+#printing the square roots
+squareRoot=number**2
+print("square root of a number is",squareRoot)
