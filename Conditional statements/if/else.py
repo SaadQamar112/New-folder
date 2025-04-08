a77717abd94b27a2741c730e85528d4f6f4401e7
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-08 19:34:56 +0500

    </>

---
+++ Conditional statements/if/else.py
@@ -0,0 +1,9 @@
+t=int(input("enter a number:"))
+if (t<15):
+    print("t is smaller then 15")
+    print("i'm  in fi block")
+else:
+    print("it is a greater number than 15")
+    print("i'm in the else block")
+print("i'm neither in if and nor in else block")
+ 
