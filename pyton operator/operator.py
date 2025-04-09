75a8bcb34f6753f8f16ca899ae9235de8fdba3eb
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-09 19:16:07 +0500

    operators

---
+++ pyton operator/operator.py
@@ -0,0 +1,17 @@
+a=13
+b=19
+c=0
+
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("one of the numbers is have boolean value as false")
+
+a=6
+b=-25
+c=0
+
+if a >0 or b>0:
+    print("either one of the number is grater than 0")
+else:
+    print("none of the number is greater than 0")
