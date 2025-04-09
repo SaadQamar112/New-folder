a78b0be6ab92e4c12ca50896c8362e7354d5388e
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-09 19:27:07 +0500

    ! not operators ;)

---
+++ pyton operator/not-operator.py
@@ -0,0 +1,18 @@
+a=9
+b=7
+c=7
+
+print(a!=b)
+print(b!=c)
+
+a="class"
+b="coding"
+
+if a!=b:
+    print("both of them are different from each other")
+
+a=97
+b=45
+
+if (a==23)!=(b==45):
+    print("hello but bye")
