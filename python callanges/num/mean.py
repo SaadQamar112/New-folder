7919fd42abc1697271b6e938503b73e316634159
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-16 19:35:13 +0500

    mean

---
+++ python callanges/num/mean.py
@@ -0,0 +1,16 @@
+mean1=48
+wrongnum=46
+correctnum=66
+totalnum=50
+
+#sum 
+
+sum=mean1*totalnum
+print(sum)
+
+num2=sum-((wrongnum)-(correctnum))
+print(num2)
+
+# finding mean
+mean2=num2/totalnum
+print(mean2)
