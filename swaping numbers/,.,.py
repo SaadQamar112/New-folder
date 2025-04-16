6116ba13301ce0ac99dabebea38a507de9e9ea3b
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-16 20:12:51 +0500

    swaping numbers

---
+++ swaping numbers/,.,.py
@@ -0,0 +1,8 @@
+# creating a project
+
+number=int(input("enter a number to swap: "))
+number2=int(input("enter a number to multip: "))
+ #shif left and right
+print("number",number>>1)
+print("number2",number2<<1)
+
