959dc351996b6ea34a0c3c707f9d2a3735be31cb
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-16 19:24:03 +0500

    num/dinomi

---
+++ python callanges/num/dinomi.py
@@ -0,0 +1,9 @@
+print("enter a number numenator")
+numn=int(input())
+print("enter a dinominater")
+numd=int(input())
+
+if numn%numd ==0:
+    print("the number is divisible")
+else:
+    print("the number is not divisible")
