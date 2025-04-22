1b117aaf360630c26766148ee38be4050c305276
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-22 19:29:54 +0500

    nestative

--- nestestive/nest.py
+++ nestestive/nest.py
@@ -1,10 +1,12 @@
 #creating variables
 
 i=(input("did you attend te the medical cost Y or N: "))
-g=(input("student medical atendence: "))
+g=int(input("student medical atendence: "))
 
 if i=="Y":
     print("allowed")
 else:
    if g>=75:
-    print("not allowed")
+    print("allowed")
+   else:
+      print("not allowed")
