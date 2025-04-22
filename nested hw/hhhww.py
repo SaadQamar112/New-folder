3d612848877493388789f509824aa6aa7c966a59
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-22 20:02:20 +0500

    age checker

---
+++ nested hw/hhhww.py
@@ -0,0 +1,8 @@
+# Get age input from the user
+age = int(input("Enter your age: "))
+
+# Check if age is between 10 and 20
+if age >= 10 and age <= 20:
+    print("Your age is between 10 and 20.")
+else:
+    print("Your age is NOT between 10 and 20.")
