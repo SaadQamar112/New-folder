a8bd2fdecc807d7e9a4285cce8e54887564a800d
SaadQamar112 <saadbinadeem@gmail.com>
2025-04-22 19:25:41 +0500

    nestestive

---
+++ nestestive/nest.py
@@ -0,0 +1,10 @@
+#creating variables
+
+i=(input("did you attend te the medical cost Y or N: "))
+g=(input("student medical atendence: "))
+
+if i=="Y":
+    print("allowed")
+else:
+   if g>=75:
+    print("not allowed")
