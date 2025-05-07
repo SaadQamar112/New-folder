4646797906cd1831e15f0d57318b485de3e22221
SaadQamar112 <saadbinadeem@gmail.com>
2025-05-07 19:35:46 +0500

    turtle.2

---
+++ turtle/turtle2.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turt =turtle.Turtle()
+
+turt.forward(100)
+turt.left(120)
+turt.forward(100)
+turt.left(120)
+turt.forward(100)
+
+turt.penup()
+turt.right(150)
+turt.forward(50)
+
+#second setup
+
+turt.pendown()
+turt.right(90)
+turt.forward(100)
+turt.right(120)
+turt.forward(100)
+turt.right(120)
+turt.forward(100)
+
+turtle.done()
