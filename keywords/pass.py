b64460025214dfbd1f97ce9f69016dad7e39a7ee
SaadQamar112 <saadbinadeem@gmail.com>
2025-05-20 19:29:39 +0500

    pass command ! !@#

---
+++ keywords/pass.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    if i % 2==0:
+        print('Approve')
+    elif i % 3==0:
+        print('Approve')
+    elif i % 5==0:
+        print('Approve')
+    else:
+        print('INVALID COMMAND CHECK IT AGAIN')
