86f79427c7537eabc92ef081b5ce4f41c6d8e232
SaadQamar112 <saadbinadeem@gmail.com>
2025-05-20 19:55:59 +0500

    !@#$$$

---
+++ BiLL and due hw/billl.py
@@ -0,0 +1,9 @@
+bill=int(input('giver your payment: ')) #input command
+due=50 #rupess
+if bill==due:
+    print('you have fully payed you bill')
+else:
+    sub=due-bill
+    print('due remaning:',sub)
+
+
