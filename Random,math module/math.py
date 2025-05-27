82e8495f5b6f524284e5cc1152ec801fa9825430
SaadQamar112 <saadbinadeem@gmail.com>
2025-05-27 19:54:00 +0500

    math module

---
+++ Random,math module/math.py
@@ -0,0 +1,9 @@
+import math
+print('ceiling value and floor value: '+str(math.ceil(23.53))+str(math.floor(34.78)))
+
+x=12
+y=-13
+
+print(''+str(math.copysign(x,y)))
+print(''+str(math.fabs(-12)))
+print(''+str(math.gcd(25,50)))
