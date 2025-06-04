202b0c69659cc7444fd6ad116512fba8eccbba55
SaadQamar112 <saadbinadeem@gmail.com>
2025-06-04 19:21:59 +0500

    tuple !@#

---
+++ Tuple/tuple.py
@@ -0,0 +1,16 @@
+tuplex=("coding",False,1234 )
+print(tuplex)
+
+tuple=(1,2,3,4,5,6)
+print(tuple)
+
+tuple1=tuple+(1,)
+print(tuple1)
+
+tuple2=(3,3,5,8,2,6,8)
+print(tuple2.count(8))
+
+tuple3=(1,5,7,8,2,9,0,3,5,7)
+_slice=tuple3[2:6]
+print(_slice)
+
