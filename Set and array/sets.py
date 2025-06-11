bd1280cd2b2647cd8c8f88676ead28b29562c5c5
SaadQamar112 <saadbinadeem@gmail.com>
2025-06-11 19:17:52 +0500

    @#$

---
+++ Set and array/sets.py
@@ -0,0 +1,17 @@
+my_set={1,2,3,4}
+print(my_set)
+
+my_set={1.2,'saad',(1,2)}
+print(my_set)
+
+my_set={1,2,3,4,5,6,6}
+print(my_set)
+
+my_set=set([1,2,3])
+print(my_set)
+
+num_set=set([1,2,3,4,5,6])
+print(num_set)
+
+num_set.pop()
+print(num_set)
