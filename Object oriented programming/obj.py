d4af167c004f534c502c5c1c271113126153f655
SaadQamar112 <saadbinadeem@gmail.com>
2025-07-01 19:20:09 +0500

    Supercars OBJ

---
+++ Object oriented programming/obj.py
@@ -0,0 +1,8 @@
+class supercar:
+    def __init__(self,maxspeed,milage):
+        self.maxspeed=maxspeed
+        self.milage=milage
+
+lambroghini_Huracine=supercar(400,24)
+print('',lambroghini_Huracine.maxspeed)
+print('',lambroghini_Huracine.milage)
