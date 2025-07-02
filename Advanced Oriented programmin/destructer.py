683f6b3902d3542ee28c8a6ed68007c1ff5d3018
SaadQamar112 <saadbinadeem@gmail.com>
2025-07-02 19:33:10 +0500

    dESTRUCTER fix+=

---
+++ Advanced Oriented programmin/destructer.py
@@ -0,0 +1,28 @@
+# Create Class
+
+class Employee:
+
+  def __init__(self):
+    print('Employee created')
+
+
+# Calling destructor
+
+def __del__(self):
+    print("Destructor called")
+
+
+def Create_obj():
+
+ print('Making Object...')
+
+obj = Employee()
+
+print('function end...')
+
+
+print('Calling Create_obj() function...')
+
+obj = Create_obj()
+
+print('Program End...')
